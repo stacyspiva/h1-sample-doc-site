9af6a0b4d72155bf875559e670c0a0d4e1f4229b
stacyspiva <36056941+stacyspiva@users.noreply.github.com>
2018-02-07 13:53:21 -0800

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -16,7 +16,7 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'HackerOne Doc Test Site' /* title for your website */,
+  title: 'Doc Test Site' /* title for your website */,
   tagline: 'HackerOne Documentation',
   url: 'https://facebook.github.io' /* your website url */,
   baseUrl: '/test-site/' /* base url for your project */,
